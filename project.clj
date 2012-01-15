d6aaae9e4a51d8645a266ceb2d24f239acda5e4b
Daniel Harper <djharperuk@gmail.com>
2012-01-15 14:41:37 +0000

    Upgraded project to use Jedis version 2.0.0 (latest release) and added new commands ZREVRANGEBYSCORE and ZREVRANGEBYSCOREWITHSCORES

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-redis "0.0.13-SNAPSHOT"
+(defproject clj-redis "0.0.14-SNAPSHOT"
   :description "Clojure Redis client library"
   :url "https://github.com/mmcgrana/clj-redis"
   :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
-                 [redis.clients/jedis "1.5.2"]]
+                 [redis.clients/jedis "2.0.0"]]
   :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]
                       [redis.clients/jedis "1.5.2"]]
                "1.2" [[org.clojure/clojure "1.2.1"]
-                      [redis.clients/jedis "1.5.2"]]}
+                      [redis.clients/jedis "2.0.0"]]}
   :dev-deps [[lein-multi "1.0.0"]])
--- readme.md
+++ readme.md
@@ -5,9 +5,9 @@ Clojure [Redis](http://redis.io) client library.
 ## Usage
 
     (require '[clj-redis.client :as redis])
-    
+
     (def db (redis/init))
-    
+
     (redis/ping db)
     => "PONG"
 
@@ -24,7 +24,7 @@ The connections represented by the return value of `clj-redis.client/init` are t
 
 ## Installation
 
-Depend on `[clj-redis "0.0.12"]` in your `project.clj`.
+Depend on `[clj-redis "0.0.14"]` in your `project.clj`.
 
 ## License
 
